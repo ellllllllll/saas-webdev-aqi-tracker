8284224237ad3703a785a14e636e1dde846bd0de
Eve Lin <elllllnn@berkeley.edu>
2020-10-19 00:18:42 -0700

    basic structure

---
+++ script.js
@@ -0,0 +1,11 @@
+const url = 
+
+fetch(url) // Call the fetch function passing the url of the API as a parameter
+.then(function() {
+    // Your code for handling the data you get from the API
+})
+.catch(function() {
+    // This is where you run code if the server returns any errors
+});
+
+
